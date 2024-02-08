456303c58ba4852691bcd41d4e7847474ecf6303
pjfirrel-su <123100210+pjfirrel-su@users.noreply.github.com>
2024-02-08 17:15:52 -0500

    attendance: rooms array

--- airbnb/js/main.js
+++ airbnb/js/main.js
@@ -1,14 +1,86 @@
-//console.log("js has been loaded");
+// console.log("js has been loaded");
 
 const menuBtn = document.getElementById("menuBtn");
 const mobileMenu = document.getElementById("mobileMenu");
 const closeBtn = document.getElementById("closeBtn")
 
 menuBtn.addEventListener("click", function() {
-    //console.log("clicked!")
+    // console.log("clicked!")
     mobileMenu.classList.add("active");
-}); //end of menuBtn click
+}); // end of menuBtn click
 
 closeBtn.addEventListener("click", function() {
     mobileMenu.classList.remove("active");
 });
+
+// variables
+// const roomName = 'Luxury King Room';
+// const roomPrice = 300;
+// const roomGuests = 2;
+// const roomDescription =
+//     'A beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.';
+
+// const room = {
+//     name: 'Luxury King Room',
+//     price: 300,
+//     guests: 2,
+//     description:
+//     'A beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.',
+// };
+
+// array of objects
+const rooms = [
+    {
+        name: 'Luxury King Room',
+        price: 300,
+        guests: 2,
+        description:
+        'A beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.',
+    },
+    {
+        name: 'Luxury King Room',
+        price: 300,
+        guests: 2,
+        description:
+        'A beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.',
+    },
+    {
+        name: 'Luxury King Room',
+        price: 300,
+        guests: 2,
+        description:
+        'A beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.',
+    },
+    
+]; // end of rooms
+
+rooms.forEach((room) => {
+
+    // create elements
+    const roomArticle = document.createElement('article');
+    roomArticle.classList.add('room');
+
+    const roomNameElement = document.createElement('h3');
+    roomNameElement.classList.add('room--name');
+    roomNameElement.textContent = room.name;
+
+    const roomDescriptionElement = document.createElement('p');
+    roomDescriptionElement.classList.add('room--description');
+    roomDescriptionElement.textContent = room.description;
+
+    const roomPriceElement = document.createElement('p');
+    roomPriceElement.textContent = `Price: ${room.price}`;
+
+    const roomGuestsElement = document.createElement('p');
+    roomGuestsElement.textContent = `Guests: ${room.guests}`;
+
+    roomArticle.appendChild(roomNameElement);
+    roomArticle.appendChild(roomDescriptionElement);
+    roomArticle.appendChild(roomPriceElement);
+    roomArticle.appendChild(roomGuestsElement);
+
+    document.body.appendChild(roomArticle);
+
+}); // end of forEach
+
+
