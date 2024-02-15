73b08fba9aa9e383a311156d5503a16c151a414e
pjfirrel-su <123100210+pjfirrel-su@users.noreply.github.com>
2024-02-15 18:22:39 -0500

    attendance Feb. 15, 2024; added filtered properties

--- airbnb/js/main.js
+++ airbnb/js/main.js
@@ -91,6 +91,58 @@ function renderProperties(properties) {
 //         renderProperties(data);
 //     });
 
+const displayCategory = (category, properties) => {
+    //console.log({category})
+    const sectionElement = document.createElement('section');
+    sectionElement.classList.add('category');
+
+    const sectionTitle = document.createElement('h2');
+    sectionTitle.textContent = category.label.plural;
+
+    sectionElement.appendChild(sectionTitle);
+    
+    console.log(category.label.singular);
+    // 1. filter properties
+    const filteredProperties = properties.filter((property) => {
+        // return true or false
+        return category.label.singular === property.type;
+    });
+
+    filteredProperties.sort((a, b) => {
+        if (a.name < b.name) {
+          return -1;
+        }
+        if (a.name > b.name) {
+          return 1;
+        }
+        return 0;
+      });
+
+    // console.log({filteredProperties})
+    filteredProperties.forEach((property) => {
+        const articleElement = document.createElement('article');
+        articleElement.classList.add('property');
+
+        // const propertyTitle = document.createElement('h3');
+        // propertyTitle.classList.add('property--title');
+
+        let propertyHtml = `
+            <h3 class="property--title">${property.name}</h3>
+            <p class="property--description">${property.description}</p>
+            <p class="property--price">${property.price}</p>
+        `;
+
+        articleElement.innerHTML = propertyHtml;
+
+        sectionElement.appendChild(articleElement);
+
+    }); // end of forEach
+
+    // 2. loop and append properties
+    
+    document.body.appendChild(sectionElement);
+} // end of displayCategory
+
 Promise.all([
     // fetch 1
     fetch('js/properties.json').then(response => response.json()),
@@ -107,15 +159,3 @@ Promise.all([
     .catch((error) => {
         console.error("There was a problem fetching the data:", error);
     });
-
-    const displayCategory = (category, properties) => {
-       //console.log({category})
-       const sectionElement = document.createElement('section');
-
-       const sectionTitle = document.createElement('h2');
-       sectionTitle.textContent = category.label.plural;
-
-       sectionElement.appendChild(sectionTitle);
-
-       document.body.appendChild(sectionElement);
-    } // end of displayCategory
