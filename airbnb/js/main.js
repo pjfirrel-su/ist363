26cf75903ae2309a7195831f2f973860618d97f3
pjfirrel-su <123100210+pjfirrel-su@users.noreply.github.com>
2024-02-20 18:22:17 -0500

    attendance 02/20/24: practiced filter array method

--- airbnb/js/main.js
+++ airbnb/js/main.js
@@ -13,46 +13,6 @@ closeBtn.addEventListener("click", function() {
     mobileMenu.classList.remove("active");
 });
 
-// variables
-// const roomName = 'Luxury King Room';
-// const roomPrice = 300;
-// const roomGuests = 2;
-// const roomDescription =
-//     'A beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.';
-
-// const room = {
-//     name: 'Luxury King Room',
-//     price: 300,
-//     guests: 2,
-//     description:
-//     'A beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.',
-// };
-
-// array of objects
-// const rooms = [
-//     {
-//         name: 'Luxury King Room',
-//         price: 300,
-//         guests: 2,
-//         description:
-//         'A beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.',
-//     },
-//     {
-//         name: 'Luxury King Room',
-//         price: 300,
-//         guests: 2,
-//         description:
-//         'A beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.',
-//     },
-//     {
-//         name: 'Luxury King Room',
-//         price: 300,
-//         guests: 2,
-//         description:
-//         'A beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.',
-//     },
-// ]; // end of rooms
-
 function renderProperties(properties) {
     properties.forEach((room) => {
 
---
+++ airbnb/js/oldcode.js
@@ -0,0 +1,39 @@
+// variables
+// const roomName = 'Luxury King Room';
+// const roomPrice = 300;
+// const roomGuests = 2;
+// const roomDescription =
+//     'A beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.';
+
+// const room = {
+//     name: 'Luxury King Room',
+//     price: 300,
+//     guests: 2,
+//     description:
+//     'A beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.',
+// };
+
+// array of objects
+// const rooms = [
+//     {
+//         name: 'Luxury King Room',
+//         price: 300,
+//         guests: 2,
+//         description:
+//         'A beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.',
+//     },
+//     {
+//         name: 'Luxury King Room',
+//         price: 300,
+//         guests: 2,
+//         description:
+//         'A beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.',
+//     },
+//     {
+//         name: 'Luxury King Room',
+//         price: 300,
+//         guests: 2,
+//         description:
+//         'A beautiful room with a king size bed, a private bathroom, and a balcony with a view of the ocean.',
+//     },
+// ]; // end of rooms
